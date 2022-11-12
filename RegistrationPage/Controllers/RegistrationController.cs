faf8652e6284001903282850c0ff112ffa426bb5
cemil <cemiw033@gmail.com>
2022-11-12 14:41:46 +0400

    registr

---
+++ RegistrationPage/Controllers/RegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationPage.Models;
+
+namespace RegistrationPage.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User users)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            return Redirect("Registration/Welcome");
+        }
+    }
+}
---
+++ RegistrationPage/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationPage.Models.ModelMetadataTypes;
+
+namespace RegistrationPage.Models
+{
+    [ModelMetadataType(typeof(UserValidator))]
+    public class User
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
